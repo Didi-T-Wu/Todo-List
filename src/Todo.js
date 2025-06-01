0ac1a15fe54ffbe86b5d429b27f0ea056bd2b98d
Tichin Wu <twu38@mail.ccsf.edu>
2025-06-01 14:59:03 -0700

    Add deadline to todo

--- src/Todo.js
+++ src/Todo.js
@@ -3,13 +3,25 @@ import React from "react";
 /** Simple presentation component for a todo.
  *
  * Props:
- * - todo: like { id, title, description, priority, isCompleted }
+ * - todo: like { id, title, description, priority, isCompleted, deadline }
  *
  * { EditableTodo, TopTodo } -> Todo
  **/
-
+// TODO: make this checkbox toggle the isCompleted state
 function Todo({ todo, toggleComplete }) {
-  const { title, description, priority, isCompleted } = todo;
+  const { title, description, priority, isCompleted, deadline } = todo;
+  const renderPriority = () => {
+    switch (priority) {
+      case 1:
+        return "High";
+      case 2:
+        return "Medium";
+      case 3:
+        return "Low";
+      default:
+        return "Unknown";
+    }
+  }
 
   return (
     <div className="Todo">
@@ -23,7 +35,7 @@ function Todo({ todo, toggleComplete }) {
         >
           <b>{title}</b>
         </div>
-        <small>Priority: {priority}</small>
+        <small>Priority: {renderPriority()}</small>
       </div>
       <div>
         <small>{description}</small>
