c717e4c369e8ef712ae7ab043f4b61a2bc17a3d7
Tichin Wu <twu38@mail.ccsf.edu>
2025-06-17 23:13:30 -0700

    Change todo to to-do

--- src/App.js
+++ src/App.js
@@ -12,12 +12,14 @@ import "./styles/style.scss";
  *
  * App -> TodoApp
  **/
+const TODAY = new Date(new Date().toLocaleDateString()).toISOString().slice(0, 10);
 function App() {
   return (
     <main className="container-fluid bg-secondary min-vh-100 d-flex flex-column">
       <header className="text-center py-3 text-light">
-        <h1>My Todos</h1>
+        <h1>My To-Dos</h1>
         <p>Manage your todos efficiently!</p>
+        <p>Today's date: {TODAY}</p>
       </header>
       <div className="container mb-2 bg-white rounded shadow-lg p-3">
         <TodoApp initialTodos={[]} />
